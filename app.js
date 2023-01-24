2823a95714eb488543a9060e4499d2986675f3f6
vishnuindira <vishnuindira123@gmail.com>
2023-01-24 21:02:29 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello, World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+module.exports = app;
